b0a91ccaf2f93d71f537f1566731ca68e39ba210
Evan <nyxora1@gmail.com>
2026-02-20 12:46:15 +0700

    level 4.

--- Assets/Evan UI Kits/Scripts/UI Navigator/quit.cs
+++ Assets/Evan UI Kits/Scripts/UI Navigator/quit.cs
@@ -1,13 +1,20 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class quit : MonoBehaviour
+[RequireComponent(typeof(Collider))]
+public class quit : MonoBehaviour, IPointerDownHandler
 {
     public void quitFunction()
     {
+        Debug.Log("Quit application requested.");
+        #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+        #else
         Application.Quit();
+        #endif
     }
 
-    public void OnMouseDown()
+    public void OnPointerDown(PointerEventData eventData)
     {
         quitFunction();
     }
